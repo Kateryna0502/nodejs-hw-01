33379da7d4914a82feeb8a2896d91f036a0927fd
Kateryna <mweoliss74@gmail.com>
2024-10-01 01:29:06 +0300

    v

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,2 +1,7 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-export const PATH_DB = 'src/db/db.json';
+const __filename = fileURLToPath(import.meta.url); // Отримуємо шлях до поточного файлу
+const __dirname = path.dirname(__filename); // Отримуємо шлях до директорії
+
+export const PATH_DB = path.join(__dirname, '../db/db.json');
