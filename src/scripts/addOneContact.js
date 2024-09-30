82568a5687c764425cdcd36838d3e48fa4e09f98
Kateryna <mweoliss74@gmail.com>
2024-10-01 01:19:43 +0300

    п

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,18 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const addOneContact = async () => { 
+    try {
+    const newContact = createFakeContact();
+    const oldContacts = await readContacts();
+    const updatedContacts = [...oldContacts, newContact];
+
+    await writeContacts(updatedContacts);
+    console.log('Successfully added one new contact.');
+  } catch (error) {
+    console.error('Error adding one contact:', error);
+  }
+};
 
 addOneContact();
